d14ecda4ecb52eb3ae9855684a6fc0ea1a51a3aa
Aparna Pal <aparna.pal.1994@gmail.com>
2014-07-21 14:41:39 -0500

    Create a helper class for FDeltaUp and FDeltaDown
    The functions for FDeltaUp and FDeltaDown are nearly identical. The
    only difference between the two is which image is subtracted from the
    other (in other words, whether the resulting image will be positive or
    negative). Since the the rest of the code for the two classes is
    identical, it seems prudent to create a helper class for both FDeltaUp
    and down to rely on. By specifying to Delta (the helper class) whether
    it is being called by FDeltaUp or FDeltaDown, it affects the order of
    the image subtraction.

---
+++ src/main/java/Delta.java
@@ -0,0 +1,77 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.WindowManager;
+import ij.process.ImageProcessor;
+
+/** Helper class for {@link Delta_F_up} and {@link Delta_F_down}. */
+public class Delta {
+
+	public void run(final boolean isUp) {
+		final ImagePlus imp = WindowManager.getCurrentImage();
+		if (imp == null) {
+			IJ.noImage();
+			return;
+		}
+
+		final ImageStack stack1 = imp.getStack();
+		final String fileName = imp.getTitle();
+		final int endslice = stack1.getSize();
+
+		final ImagePlus imp2 = duplicateStack(imp);
+		imp2.show();
+		// IJ.showMessage("Box",fileName);
+		final ImageStack stack2 = imp2.getStack();
+		stack1.deleteSlice(1);
+		stack2.deleteSlice(endslice);
+
+		final String image2 = imp2.getTitle();
+
+		final String calculatorstring = calculateString(fileName, image2, isUp);
+
+		IJ.run("Image Calculator...", calculatorstring);
+		final ImagePlus imp3 = WindowManager.getCurrentImage();
+		imp3.setTitle(fileName + " DeltaF " + (isUp ? "up" : "down"));
+		imp2.getWindow().close();
+		imp.getWindow().close();
+
+	}
+
+	ImagePlus duplicateStack(final ImagePlus img1) {
+		final ImageStack stack1 = img1.getStack();
+		final int n = stack1.getSize();
+		ImageStack stack2 = img1.createEmptyStack();
+		try {
+			for (int i = 1; i <= n; i++) {
+				final ImageProcessor ip1 = stack1.getProcessor(i);
+				ip1.resetRoi();
+				final ImageProcessor ip2 = ip1.crop();
+				stack2.addSlice(stack1.getSliceLabel(i), ip2);
+			}
+		}
+		catch (final OutOfMemoryError e) {
+			stack2.trim();
+			stack2 = null;
+			return null;
+		}
+		return new ImagePlus("Duplicate", stack2);
+	}
+
+	public String calculateString(final String image1, final String image2,
+		final boolean isUp)
+	{
+		String calculatorstring = null;
+		if (isUp) {
+			calculatorstring =
+				("image1='" + image1 + "' operation=Subtract image2=" + image2 + " create stack");
+
+		}
+		else {
+			calculatorstring =
+				("image1='" + image2 + "' operation=Subtract image2=" + image1 + " create stack");
+		}
+
+		return calculatorstring;
+
+	}
+}
